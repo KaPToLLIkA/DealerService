1d92aa348ca24e58257f56110b31655509308460
ST/HEkTO <eduardpur18@gmail.com>
2024-02-02 22:57:03 +0300

    refactor base repository class, added sql select query interface

---
+++ DealerPersonalAccount/Models/Repository/Base/ARepository.cs
@@ -0,0 +1,44 @@
+﻿using DealerPersonalAccount.Connections;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace DealerPersonalAccount.Models.Repository.Base
+{
+    public abstract class ARepository<T>
+    {
+        private ISqlConnectionProvider _connectionProvider;
+
+        public ARepository(ISqlConnectionProvider connectionProvider)
+        {
+            _connectionProvider = connectionProvider;
+        }
+
+        protected void ExecuteQuery(Action<SqlConnection> query)
+        {
+            try
+            {
+                using (SqlConnection connection = _connectionProvider.Provide())
+                {
+                    query(connection);
+                }
+            }
+            catch (TimeoutException ex)
+            {
+                throw new Exception($"{GetType().FullName}.{nameof(ExecuteQuery)} " +
+                    $"experienced a SQL timeout", ex);
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception($"{GetType().FullName}.{nameof(ExecuteQuery)} " +
+                    $"experienced a SQL exception", ex);
+            }
+        }
+
+        public abstract void Insert(T entity);
+        public abstract void Remove(T entity);
+        public abstract void Update(T entity);
+
+        public abstract IEnumerable<T> Select(ISqlSelectQuery<T> specification);
+    }
+}
---
+++ DealerPersonalAccount/Models/Repository/Base/ISqlSelectQuery.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace DealerPersonalAccount.Models.Repository.Base
+{
+    public interface ISqlSelectQuery<T>
+    {
+        IEnumerable<T> SelectFrom(SqlConnection connection);
+    }
+}
